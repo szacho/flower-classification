6ad3be5586638135d9503f7dfbb096633e91511d
Michał Szachniewicz <mszachniewicz@outlook.com>
2020-04-16 21:37:33 +0200

    Add TPU initialization

---
+++ tpu_init.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+def initialize_tpu():
+    # Detect hardware, return appropriate distribution strategy
+    try:
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection. No parameters necessary if TPU_NAME environment variable is set. On Kaggle this is always the case.
+        print('Running on TPU ', tpu.master())
+    except ValueError:
+        tpu = None
+
+    if tpu:
+        tf.config.experimental_connect_to_cluster(tpu)
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        strategy = tf.distribute.experimental.TPUStrategy(tpu)
+    else:
+        strategy = tf.distribute.get_strategy() # default distribution strategy in Tensorflow. Works on CPU and single GPU.
+
+    print("REPLICAS: ", strategy.num_replicas_in_sync)
+    return strategy
